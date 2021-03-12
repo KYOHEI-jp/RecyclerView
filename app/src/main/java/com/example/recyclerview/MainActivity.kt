1e63aa379c117c3515595634b117782030af9777
KYOHEI <watanabekyouhei@watanabekyouheinoMacBook.local>
2021-03-13 01:35:52 +0900

    modify

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -9,15 +9,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movie = Movie("Captain America", 9)
-        var movieList: ArrayList<Movie> = ArrayList()
-        movieList.add(Movie("Iron man", 5))
-        movieList.add(Movie("Thor", 9))
-        movieList.add(Movie("Incredible Hulk", 4))
-        movieList.add(Movie("Guardians", 3))
-        movieList.add(Movie("Avengers", 8))
-        movieList.add(Movie("Avengers: Age of Ultron", 6))
-        movieList.add(Movie("Thor: The Dark World", 7))
+        val movieList = arrayListOf<Movie>(
+            Movie("Captain America", 9),
+            Movie("Iron man", 5),
+            Movie("Thor", 9),
+            Movie("Incredible Hulk", 4),
+            Movie("Guardians", 3),
+            Movie("Avengers", 8),
+            Movie("Avengers: Age of Ultron", 6),
+            Movie("Thor: The Dark World", 7)
+
+        )
 
         Log.d("MainActivity", movieList.toString())
 
